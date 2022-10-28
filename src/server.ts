f9f1a7ad1182f6e032ff28823264b7cf5c908bda
aleksastojsic <stojsic.aleksa@gmail.com>
2022-10-28 15:44:15 +0200

    Fix: trying vercel for the client

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: 'https://react-chat-app-51bh.onrender.com'
+    origin: 'https://chat-socketio-react.vercel.app'
   }
 });
 
