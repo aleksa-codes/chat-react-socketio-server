e8256882be2f751fbdf1903e8c614d60d8abf8a0
aleksastojsic <stojsic.aleksa@gmail.com>
2022-10-28 16:36:30 +0200

    Fix: client domain change

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: 'https://chat-socketio-react.vercel.app'
+    origin: 'https://chat.aleksa.codes'
   }
 });
 
